9180413f0b91977b89c869165c1fecfb469d7f87
Rodrigo Alves <rodrigoaes.20@hotmail.com>
2022-09-13 17:19:15 -0300

    Adicionados os arquivos LoginController.php e LoginHandler.php e também o model User.php para adicionar a função de verificação de login da sessão

---
+++ src/controllers/LoginController.php
@@ -0,0 +1,15 @@
+<?php
+namespace src\controllers;
+
+use \core\Controller;
+
+class LoginController extends Controller {
+
+    public function singin() {
+        echo 'login';
+    }
+    public function singup() {
+        echo 'cadastro';
+    }
+
+}
